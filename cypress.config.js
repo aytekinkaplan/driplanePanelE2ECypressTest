1b626e991f9be0c1b6003037da2c2cafc0367627
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-04-05 22:18:49 +0300

    New Changes have committed

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,11 @@
-const { defineConfig } = require('cypress')
+const {defineConfig} = require('cypress')
 
 module.exports = defineConfig({
-  e2e: {
-    // Configure your E2E tests here
-    //specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
-    baseUrl: 'http://localhost:4200'
+    e2e: {
+        // Configure your E2E tests here
+        specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}",
+        baseUrl: 'http://localhost:4200'
 
 
-  },
+    },
 })
